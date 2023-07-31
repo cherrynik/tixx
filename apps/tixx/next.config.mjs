a570cd7047e40ba48c59bbd8c0c943e55601e998
Nik Cherednik <me@cherrynik.ru>
2023-07-31 14:11:41 +0300

    feat(infra): env validation

--- apps/tixx/next.config.mjs
+++ apps/tixx/next.config.mjs
@@ -1,7 +1,6 @@
 //@ts-check
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { composePlugins, withNx } = require('@nx/next');
+import "./app/env/server.mjs"
+import { composePlugins, withNx } from '@nx/next';
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -10,12 +9,13 @@ const nextConfig = {
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
-    svgr: false,
+    svgr: true,
   },
 
   compiler: {
     // For other options, see https://styled-components.com/docs/tooling#babel-plugin
     styledComponents: true,
+    removeConsole: true
   },
 };
 
@@ -24,4 +24,4 @@ const plugins = [
   withNx,
 ];
 
-module.exports = composePlugins(...plugins)(nextConfig);
+export default composePlugins(...plugins)(nextConfig);
